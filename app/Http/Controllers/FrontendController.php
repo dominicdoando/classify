f86e65e385277c8d729427886f170e38a5bf6f3a
dominicdoan2803 <ddo@hthcomm.com>
2021-08-26 04:30:53 +0700

    filter by price

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -9,10 +9,19 @@ use App\Models\Subcategory;
 use App\Models\ChildCategory;
 class FrontendController extends Controller
 {
-    public function findBaseOnSubcategory($categorySlug,Subcategory $subcategorySlug){
-        $advertisements = $subcategorySlug->ads;
+    public function findBaseOnSubcategory(Request $request,$categorySlug,Subcategory $subcategorySlug){
+
+        $advertisementBaseOnFilter = Advertisement::where('subcategory_id', $subcategorySlug->id)->when($request->minPrice, function($query, $minPrice) {
+            return $query->where('price','>=' ,$minPrice);
+        })->when($request->maxPrice, function($query, $maxPrice) {
+            return $query->where('price','<=', $maxPrice);
+        })->get();
+        $advertisementWithoutFilter = $subcategorySlug->ads;//NO CHOOSE PRICE
         $filterByChildCategories = $subcategorySlug->ads->unique('childcategory_id');
+        $advertisements = $request->minPrice||$request->maxPrice?$advertisementBaseOnFilter:$advertisementWithoutFilter;// CHOOSE PRICE
+
         return view('product.subcategory', compact('advertisements', 'filterByChildCategories'));
+
     }
     public function findBaseOnChildcategory(
         $categorySlug,
--- resources/views/product/subcategory.blade.php
+++ resources/views/product/subcategory.blade.php
@@ -5,14 +5,28 @@
     <div class="container">
         <div class="row">
             <div class="col-md-3">
-                <div class="card">
+                <div class="card pl-2 pr-2">
                         <div class="list-group">
                             @foreach ($filterByChildCategories as $ad)
-                            @php
-                            @endphp
                             <a href="{{ url()->current()}}/{{($ad->childcategory->slug)??'' }}" class="list-group-item">{{$ad->childcategory->name??''}}</a>
                             @endforeach
                         </div>
+                        <br>
+                        <form action="{{ url()->current() }}" method="get">
+                            <div class="form-group">
+                              <label for="">Minumum Price</label>
+                              <input type="text"
+                                class="form-control" name="minPrice" placeholder="">
+                            </div>
+                            <div class="form-group">
+                                <label for="">Maximum Price</label>
+                                <input type="text"
+                                  class="form-control" name="maxPrice" placeholder="">
+                              </div>
+                              <div class="form-group">
+                                <button class="btn btn-danger">Search</button>
+                              </div>
+                        </form>
                   </div>
             </div>
             <div class="col-md-9">
